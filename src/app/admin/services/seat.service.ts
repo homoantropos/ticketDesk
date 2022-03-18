3256009134fe25960bb8ba03003a397d75c6a477
Segii Antropov <homoantropos@gmail.com>
2022-03-18 20:09:18 +0200

    seats editor processed

---
+++ src/app/admin/services/seat.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Seat} from "../../shared/interfaces";
+import {environment} from "../../../environments/environment";
+import {ActivatedRoute} from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeatService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getSeats(id: number): Observable<Array<Seat>> {
+    return this.http.get<Array<Seat>>(`${environment.dbUrl}/seat?venueId=${id}`);
+  }
+}
